db91d5a8eb7fa05a49b770580ab1451435c2c32f
Remario Tenkiang <santosdangote09@gmail.com>
2023-04-03 17:47:32 -0400

    update count

--- provider.tf
+++ provider.tf
@@ -1,9 +1,13 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-  count         = 3
-
-  tags = {
-    Name = "ExampleAppServerInstance"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
   }
+
+  required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+  region  = "us-west-2"
