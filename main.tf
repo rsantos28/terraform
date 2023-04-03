1a18b1131f7e47710808c994e403d13e6352a641
Remario Tenkiang <santosdangote09@gmail.com>
2023-04-03 18:54:49 -0400

    updated

--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
 
 
 resource "aws_instance" "app_server" {
-  ami           = "var.image_id"
-  instance_type = "var.instance_type"
+  ami           = var.image_id
+  instance_type = var.instance_type
   count         = 3
-
   tags = {
-    Name = "var.env"
+    Name = var.env
   }
 }
 resource "aws_s3_bucket" "b" {
   bucket = var.bucketname
   tags = {
     Name        = "My tf bucket"
-    Environment = "var.env"
+    Environment = var.env
   }
 }
